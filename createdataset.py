27d3d16de5ee61e25d589cd68d1f6d84a1fd7d65
Rien Boonstoppel <rien.boonstoppel@gmail.com>
2020-06-08 21:48:02 +0200

    add content

---
+++ createdataset.py
@@ -0,0 +1,83 @@
+import csv
+import numpy as np
+import time
+start_time = time.time()
+
+
+def loadData(file):
+    ''' load the initial data, and calculate the mean of every gene over the 100 experiments'''
+    tsv_file = open(file)
+    read_tsv = csv.reader(tsv_file, delimiter = "\t")
+    KOdata = np.array(list(read_tsv)[1:]).astype("float")
+    tsv_file.close()
+    return KOdata
+
+def loadGSdata(file,size):
+    tsv_file = open(file)
+    read_tsv = csv.reader(tsv_file, delimiter = "\t")
+    GSraw = list(read_tsv)
+    
+    GSreshaped = np.zeros([size, size])
+    GS = []
+
+    for i in range(len(GSraw)):
+        GS.append([s.strip('G') for s in GSraw[i]])
+    
+    for i in range(len(GS)):
+        x = int(GS[i][0])-1
+        y = int(GS[i][1])-1
+        edge = float(GS[i][2])
+        GSreshaped[y][x] = edge
+    
+    return GSreshaped
+
+def dataprep(KOdata,WTdata,size):
+    dataset = np.array([])
+    for j in range(len(KOdata)):
+        for i in range(len(KOdata)):
+            dataset = np.append(dataset, KOdata[:,i])
+            dataset = np.append(dataset, KOdata[i,:])
+            dataset = np.append(dataset, KOdata[:,j])
+            dataset = np.append(dataset, KOdata[j,:])
+            dataset = np.append(dataset, WTdata[0,i])
+            dataset = np.append(dataset, WTdata[0,j])
+    return dataset
+
+def openFiles(size, number):
+    cur_path = r'C:\\Users\\Rien\\CloudDiensten\\Stack\\Documenten\\Python Scripts\\gnw-master'
+    new_path = cur_path + '\\10times50_1'
+    name = '\\Yeast-' + str(number) 
+    KOpath = new_path + name + '_knockouts.tsv'
+    WTpath = new_path + name + '_wildtype.tsv'
+    GSpath = new_path + name + '_goldstandard.tsv'
+    
+    KOdata = loadData(KOpath)
+    WTdata = loadData(WTpath)
+    GSdata = loadGSdata(GSpath, size)
+    
+    return KOdata, WTdata, GSdata
+
+complete_dataset=np.array([])
+labels = []
+
+for i in range(10):
+    source = 'GNW'
+    size = 50
+    number = i + 1
+    KOdata, WTdata, GSdata = openFiles(size,number)
+    dataset = dataprep(KOdata,WTdata,GSdata)
+    complete_dataset =np.append(complete_dataset,dataset)
+    for k in range(len(KOdata)):
+        for l in range(len(KOdata)):
+            labels.append(GSdata[l,k])
+    print(i+1)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+length = 4*size+2
+dataset = complete_dataset.reshape([size*size*10,length])
+
+np.savetxt('dataset.txt', dataset)
+np.savetxt('labels.txt', labels)
+
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ kerasmodel.py
@@ -0,0 +1,45 @@
+import csv
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow.keras.backend as K
+
+def get_f1(y_true, y_pred): #taken from old keras source code
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+dataset = np.loadtxt('dataset.txt')
+labels = np.loadtxt('labels.txt')
+
+# Define Sequential model with 3 layers
+model = keras.Sequential(
+    [
+        layers.Dense(12, input_shape = (len(dataset[0]), ), activation="relu", name="layer1"),
+        layers.Dense(8, activation="relu", name="layer2"),
+        layers.Dense(1, activation="relu", name="layer3"),
+    ]
+)
+
+model.summary()
+
+class_weight = {0: 1.,
+                1: 25.,
+                }
+model.compile(
+    loss = keras.losses.BinaryCrossentropy(
+        from_logits=False, label_smoothing=0, reduction="auto", name="binary_crossentropy"
+        ),
+    optimizer = keras.optimizers.RMSprop(),
+    metrics=[get_f1],
+)
+
+history = model.fit(dataset, labels, batch_size=32, epochs=100, class_weight=class_weight, validation_split=0.2)
+
+test_scores = model.evaluate(dataset, labels, verbose=2)
+print("Test loss:", test_scores[0])
+print("Test accuracy:", test_scores[1])
